2d2d5ac7988b9ce52f55d151cfe76190ef020455
TNTSuperMan <tntsuperga@gmail.com>
2024-06-11 20:46:57 +0900

    dist

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,22 @@ export default defineConfig({
     vue(),
     VueDevTools(),
   ],
+  root: "src",
+  base: "./",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: "src/index.html",
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]"
+      }
     }
   }
 })
